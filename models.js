66970aa7d4a8ffbe445a3feb288f4b725e045d1c
lpost45 <lpost45@live.com>
2024-05-12 21:28:09 -0600

    Update models.js
    Password hashing/validation

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 let movieSchema = mongoose.Schema({
     Title: {type: String, required: true},
     Description: {type: String, required: true},
@@ -20,6 +21,12 @@ let userSchema = mongoose.Schema({
     Birthday: Date,
     FavMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(passord, 10);
+};
+userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.Password);
+};
 let directorSchema = mongoose.Schema({
     Name: {type: String, required: true},
     Bio: {type: String, require: true},
