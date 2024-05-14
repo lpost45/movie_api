69083f26777a7469f03cb12175ab0ccab5c53d67
lpost45 <lpost45@live.com>
2024-05-13 19:27:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Directors = Models.Director;
 const Genres = Models.Genre;
 
 //mongoose.connect('mongodb://localhost:27017/mfDB', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect( "mongodb+srv://lpost45:BeAvEr2017.@myflixdb.rue04nf.mongodb.net/myFlixDB?retryWrites=true&w=majority&appName=myflixDB", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( "mongodb+srv://lpost45:BeAvEr2017.@myflixdb.rue04nf.mongodb.net/myFlixDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express'),
     morgan = require('morgan'),
